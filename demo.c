4a1135f86d74b1492db1faf8010d59bd2a895a0e
xdai02 <2513276112@qq.com>
2023-05-31 12:44:18 +0800

    demo updated

--- demo.c
+++ demo.c
@@ -3,18 +3,17 @@
 #include "datetime.h"
 
 int main() {
-    printf("\n==================== TODAY ====================\n");
     Date today = date_now();
     char *today_str = date_to_string(today);
     printf("Today is %s (%s)\n", today_str, day_name(nth_day_of_week(today.year, today.month, today.day)));
     free(today_str);
+    printf("\n");
 
-    printf("\n==================== CALENDAR ====================\n");
     char *calendar_str = calendar(today.year, today.month);
     printf("%s\n", calendar_str);
     free(calendar_str);
+    printf("\n");
 
-    printf("\n==================== DATE ====================\n");
     Date date = date_add(today, 15);
     char *date_str = date_to_string(date);
     printf("15 days later is %s (%s)\n", date_str, day_name(nth_day_of_week(date.year, date.month, date.day)));
@@ -27,7 +26,6 @@ int main() {
     free(date1_str);
     free(date2_str);
 
-    printf("\n==================== TIME ====================\n");
     Time now = time_now();
     char *now_str = time_to_string(now);
     printf("The current time is %s\n", now_str);
@@ -44,6 +42,21 @@ int main() {
     printf("The difference between %s and %s is %d milliseconds\n", time1_str, time2_str, time_difference);
     free(time1_str);
     free(time2_str);
+    printf("\n");
+
+    DateTime datetime1 = datetime_create(today.year, today.month, today.day, now.hour, now.minute, now.second, now.millisecond);
+    char *datetime1_str = datetime_to_string(datetime1);
+    printf("The current datetime is %s\n", datetime1_str);
+
+    DateTime datetime2 = datetime_create(2024, JAN, 1, 0, 0, 0, 0);
+    TimeInterval time_interval = datetime_diff(datetime1, datetime2);
+    char *datetime2_str = datetime_to_string(datetime2);
+    char *time_interval_str = time_interval_to_string(time_interval);
+    printf("Time interval between %s and %s is %s\n", datetime1_str, datetime2_str, time_interval_str);
+    free(datetime1_str);
+    free(datetime2_str);
+    free(time_interval_str);
+    printf("\n");
 
     return 0;
 }
